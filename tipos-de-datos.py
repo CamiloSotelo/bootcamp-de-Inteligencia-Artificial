12e7c598a116232130ace49f515320911de4b6ff
Camilo Sotelo <juanrojaspasto1@gmail.com>
2024-09-07 09:28:40 -0500

    abordamos tipos de datos

---
+++ tipos-de-datos.py
@@ -0,0 +1,14 @@
+"string"#string se utiliza para cadenas de texto
+'string'#asi tambien podemos generar un string
+"""tus datos son:
+    Nombre:Camilo
+    Apellido:Sotelo"""#asi generamos un bloque de texto
+'''tus datos son:
+    Nombre:Camilo
+    Apellido:Sotelo'''#con comilla simple tambien podemos generar un bloque de texto
+#en python tenemos diferentes tipos de numeros
+4 #numero entero
+40.2 #numero tipo float
+#el tipo de datos boolean
+True #Verdadero
+False #Falso
